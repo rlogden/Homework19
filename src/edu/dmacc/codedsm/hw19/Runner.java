7d303a5b1904a48bc05490f19763f3b9f6436f7b
Rachel Ogden <rlogden@dmacc.edu>
2019-04-24 10:14:45 -0500

    Second commit

--- src/edu/dmacc/codedsm/hw19/Runner.java
+++ src/edu/dmacc/codedsm/hw19/Runner.java
@@ -7,15 +7,17 @@ import java.util.function.Supplier;
 
 public class Runner {
 
-    public static Function<String, Integer> myFunction = s -> s.length();
-    public static BiFunction<String, String, Integer> myBiFunction = (a, b) -> a.length() + b.length();
-    public static Consumer<String> myConsumer = x -> System.out.println(x);
-    public static Supplier<String> mySupplier = () -> "Hello!";
-
     public static void main(String[] args) {
+
+        Function<String, Integer> myFunction = s -> s.length();
+        BiFunction<String, String, Integer> myBiFunction = (a, b) -> a.length() + b.length();
+        Consumer<String> myConsumer = x -> System.out.println(x);
+        Supplier<String> mySupplier = () -> "Hello!";
+
         System.out.println(myFunction.apply("First"));
         System.out.println(myBiFunction.apply("Second", "Third"));
         myConsumer.accept("Fourth");
         System.out.println(mySupplier.get());
+
     }
 }
